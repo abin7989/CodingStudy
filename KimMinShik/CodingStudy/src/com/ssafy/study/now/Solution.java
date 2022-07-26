5d1139666eaa386e8ed093d264bbcfbee4f376ed
unknown <abin7989@gmail.com>
2022-07-26 13:03:13 +0900

    SWEA_Num2805_Complete

--- KimMinShik/CodingStudy/src/com/ssafy/study/now/Solution.java
+++ KimMinShik/CodingStudy/src/com/ssafy/study/now/Solution.java
@@ -1,43 +1,36 @@
 package com.ssafy.study.now;
 
-import java.awt.Point;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Solution {
-	public static void main(String args[]) throws Exception {
+public class Solution {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		
-		for(int test_case =1;test_case<=T;test_case++) {
+		for(int test_case=1;test_case<=T;test_case++) {
 			int n = Integer.parseInt(br.readLine());
-			int map[][] = new int [n][n];
-			boolean v[][] = new boolean [n][n];
+			String map[] = new String[n];
+			StringTokenizer st = new StringTokenizer( br.readLine());
 			for(int i = 0 ; i < n ; i++) {
-				String st = br.readLine();
-				for(int j = 0,index = 0; j< n ; j++,index+=2) {
-					map[i][j] = st.charAt(index)-'0';
-				}
+				map[i] = st.nextToken();
 			}
-			
-			int dx[] ={1,0,-1,0};
-			int dy[] = {0,1,0,-1};
-			int cnt = 0;
-			int posx[] = {n,}
-			for(int i = 0 ;i<4;i++) {
-				int x = n/2;
-				int y =n/2;
-				for(int j = 1;j<=n/2;j++) {
-					int nx = x+dx[i];
-					int ny = y+dy[i];
-					v[nx][ny] = true;
-					cnt+= map[nx][ny];
-				}
+			System.out.printf("#%d ",test_case);
+			int p = 0;
+			boolean pass = true;
+			if(n%2!=0) {
+				p=1;
+				pass = false;
 			}
-			
-			System.out.printf("#%d %d\n",test_case);
+			int pos = n/2+p;
+			for(int i = 0; i<n/2;i++) {
+				System.out.printf("%s ",map[i]);
+				System.out.printf("%s ",map[pos++]);
+			}
+			if(!pass)
+				System.out.printf("%s ",map[n/2]);
+			System.out.println();
 		}
-		
 	}
 }
