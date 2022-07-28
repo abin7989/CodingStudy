f44f958e08fae1c0866ea009e083d418eababbcb
suhyeon0513 <90120036+suhyeon0513@users.noreply.github.com>
2022-07-28 12:32:23 +0900

    SWEA_1289_원재의 메모리 복구하기
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV19AcoKI9sCFAZN

--- SongSuHyeon/1주차/SWEA_1289.java
+++ SongSuHyeon/1주차/SWEA_1289.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Arrays; 
  
 public class SWEA_1289 {
     public static void main(String[] args) throws NumberFormatException, IOException {
