0beae7ff02dbfd4d38cdc538c04078f744413d40
suhyeon0513 <90120036+suhyeon0513@users.noreply.github.com>
2022-08-02 22:32:09 +0900

    SWEA_2805_농작물 수확하기
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GLXqKAWYDFAXB

--- SongSuHyeon/1주차/SWEA_2805.java
+++ SongSuHyeon/1주차/SWEA_2805.java
@@ -1,5 +1,3 @@
-package day_0802;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,46 +5,39 @@ import java.io.InputStreamReader;
 public class SWEA_2805 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int T = Integer.valueOf(br.readLine());
-		int N = Integer.valueOf(br.readLine());
-		int[][] farm = new int[N][N];
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.valueOf(br.readLine());
+			int[][] farm = new int[N][N];
 
-		for (int i = 0; i < N; i++) {
-			String s = br.readLine();
-			for (int j = 0; j < N; j++) {
-				farm[i][j] = Character.getNumericValue(s.charAt(j));
+			for (int i = 0; i < N; i++) {
+				String s = br.readLine();
+				for (int j = 0; j < N; j++) {
+					farm[i][j] = Character.getNumericValue(s.charAt(j));
+				}
 			}
-		}
 
-		int half = (int) ((double) farm.length / 2.0);
-		int cnt = 1;
-		int sum = 0;
-		int left = half;
-		int right = N - 1;
-		for (int i = 0; i < farm.length; i++) {
-			if (i <= half) {
+			int half = (int) ((double) farm.length / 2.0);
+			int sum = 0;
+			int left = half;
+			int right = N - 1;
+			for (int i = 0; i <= half; i++) {
 				for (int j = left; j < left + 2 * i + 1; j++) {
 					sum += farm[i][j];
-					System.out.println(farm[i][j]);
 				}
 				left--;
-			} else
-				break;
-		}
-		System.out.println("=============");
-		for (int i = 0; i < farm.length; i++) {
-			left++;
-			right--;
-			if (i > half) {
-				for (int j = left; j < right; j++) {
+			}
+			left = 0;
+			for (int i = half + 1; i < farm.length; i++) {
+				left++;
+				right--;
+				for (int j = left; j <= right; j++) {
 					sum += farm[i][j];
-					System.out.println(farm[i][j]);
 				}
 			}
-			else continue;
+			sb.append("#").append(test_case).append(" ").append(sum).append("\n");
 		}
-
-		System.out.println(sum);
-
+		System.out.println(sb);
 	}
 }
