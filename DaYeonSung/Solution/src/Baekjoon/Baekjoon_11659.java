dcd7b02d32778d4bbb5707fd3896ada1671d42c9
yoni <sdy32@naver.com>
2022-08-03 23:10:49 +0900

    Baekjoon_11659_구간합구하기4

---
+++ DaYeonSung/Solution/src/Baekjoon/Baekjoon_11659.java
@@ -0,0 +1,35 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 11659 구간 합 구하기 4
+public class Baekjoon_11659 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); // 수의 개수
+		int M = Integer.parseInt(st.nextToken()); // 구해야 하는 횟수
+		
+		st = new StringTokenizer(br.readLine());
+		int[] nums = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			nums[i] = nums[i-1] + Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			
+			sb.append(nums[end] - nums[start - 1]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
