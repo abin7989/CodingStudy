f2c8e09387e0798889cbefa4a17b9744be4a893e
yoni <sdy32@naver.com>
2022-08-03 23:44:01 +0900

    Baekjoon_11660_구간 합 구하기 5

--- DaYeonSung/Solution/src/Baekjoon/Baekjoon_11660.java
+++ DaYeonSung/Solution/src/Baekjoon/Baekjoon_11660.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 백준 11660. 구간 합 구하기 5
 public class Baekjoon_11660 {
 
 	public static void main(String[] args) throws IOException {
