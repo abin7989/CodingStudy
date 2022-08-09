4eb700ff6f0994a58e4ad24db9411c74c6a8ea35
suhyeon0513 <90120036+suhyeon0513@users.noreply.github.com>
2022-08-09 11:05:33 +0900

    SWEA_1233_[S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV141176AIwCFAYD

--- SongSuHyeon/2주차/Day 0809/SWEA_1233.java
+++ SongSuHyeon/2주차/Day 0809/SWEA_1233.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class SWEA_1233 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -12,27 +9,20 @@ public class SWEA_1233 {
 
 			int node_cnt = Integer.valueOf(br.readLine()); // 노드의 개수 - 171
 			int[] arr = new int[node_cnt + 1]; // 부모가 있는지 체크할 배열
-			Arrays.fill(arr, 0);
 			arr[0] = '1';
 			for (int i = 1; i <= node_cnt; i++) {
-				StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 입력 받기 1 - 2 3
-				int N = Integer.valueOf(st.nextToken()); // 1
-				int s = st.nextToken().charAt(0); // 연산자 or 숫자
+				StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 입력 받기 ex) 1 - 2 3
+				int N = Integer.valueOf(st.nextToken()); // 정점 번호 ex) 1
+				int s = st.nextToken().charAt(0); // 연산자 or 숫자 : 아스키 코드 값으로 받음
 				arr[N] = s; // 해당 정점에 넣기
-				if (st.countTokens() != 0 && (s == '+' || s == '-' || s == '/' || s == '*')) {
-					if (st.countTokens() == 1) {
-						arr[2 * N] = Integer.valueOf(st.nextToken());
-					} else {
-						arr[2 * N] = Integer.valueOf(st.nextToken());
-						arr[2 * N + 1] = Integer.valueOf(st.nextToken());
-
-					}
-				}
+				while(st.hasMoreTokens()) {
+					st.nextToken();
+				} 
 			}
 
 			int flag = 1;
 			for (int i = node_cnt; i >= 2; i--) {
-				// 자식이 연산자이고 부모가 숫자인 경우 연산 불가
+				// 자식이 연산자이거나 부모가 숫자인 경우 연산 불가
 				if ((arr[i] < '0' && arr[i] > '9') || (arr[i / 2] >= '0' && arr[i / 2] <= '9')) {
 					flag = 0;
 					break;
