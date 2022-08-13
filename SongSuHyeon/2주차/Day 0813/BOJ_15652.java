6741949c1daf16737df24158e5a9a93ee4804788
suhyeon0513 <90120036+suhyeon0513@users.noreply.github.com>
2022-08-14 00:15:46 +0900

    BOJ_15652_N과 M (4)
    https://www.acmicpc.net/problem/15652

---
+++ SongSuHyeon/2주차/Day 0813/BOJ_15652.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15652 {
+	static int N, M;
+	static int[] input, numbers;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		input = new int[N];
+		numbers = new int[M];
+
+		for (int i = 1; i <= N; i++) {
+			input[i - 1] = i;
+		}
+
+		comb(0, 0);
+		System.out.println(sb);
+
+	}
+
+	private static void comb(int cnt, int start) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(numbers[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = start; i < N; i++) {
+			numbers[cnt] = input[i];
+			comb(cnt + 1, i);
+		}
+
+	}
+
+}
