4f44e3bf192ab610574cba7605e07ba37e40be0b
yoni <sdy32@naver.com>
2022-08-17 22:14:55 +0900

    Baekjoon_1541_잃어버린 괄호

---
+++ DaYeonSung/Solution/src/Baekjoon/Baekjoon_1541.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1541 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str = new StringTokenizer(br.readLine(),"-");
+		
+		int result = 0;
+		for (StringTokenizer st = new StringTokenizer(str.nextToken(), "+"); st.hasMoreTokens();) {
+			result += Integer.parseInt(st.nextToken());
+		}
+		
+		while (str.hasMoreTokens()) {
+			int sum = 0;
+			for (StringTokenizer st = new StringTokenizer(str.nextToken(), "+"); st.hasMoreTokens();) {
+				sum += Integer.parseInt(st.nextToken());
+			}
+			result -= sum;
+		}
+
+		System.out.println(result);
+	}
+
+}
