26feedb495e096d393b1fb57fb7dd191f1717fe3
yoni <sdy32@naver.com>
2022-08-21 20:27:02 +0900

    Baekjoon_10872_팩토리얼

---
+++ DaYeonSung/Solution/src/Baekjoon/Baekjoon_10872.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_10872 {
+
+	static int fact (int n) {
+		if (n <= 1) {
+			return 1;
+		} else {
+			return n * fact(n-1);
+		}
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println(fact(N));
+
+	}
+
+}
