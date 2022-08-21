8c2d28f33dadddf1aba700a380f84e2dca348e44
abin7989 <20161364@vision.hoseo.edu>
2022-08-22 00:04:07 +0900

    DAY0822

---
+++ KimMinShik/CodingStudy/src/Solution_SWEA_1218_괄호짝짓기.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Solution_SWEA_1218_괄호짝짓기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = 10;
+		for(int test = 1; test <=T; test++) {
+			Stack<String> s = new Stack<String>();
+			int n = Integer.parseInt(br.readLine());
+			String p = br.readLine();
+			EXIT:for(int i = 0; i < n; i ++) {
+				char temp = p.charAt(i);
+				switch (temp) {
+				case '(':
+				case '[':
+				case '{':
+				case '<':
+					s.push(String.valueOf(temp));
+					break;
+				case ')':
+					if(s.isEmpty())
+						break EXIT;
+					if(s.peek().charAt(0)=='(') {
+						s.pop();
+					}else {
+						break EXIT;
+					}
+					break;
+				case ']':
+					if(s.isEmpty())
+						break EXIT;
+					if(s.peek().charAt(0)=='[') {
+						s.pop();
+					}else {
+						break EXIT;
+					}
+					break;
+				case '}':
+					if(s.isEmpty())
+						break EXIT;
+					if(s.peek().charAt(0)=='{') {
+						s.pop();
+					}else {
+						break EXIT;
+					}
+					break;
+				case '>':
+					if(s.isEmpty())
+						break EXIT;
+					if(s.peek().charAt(0)=='<') {
+						s.pop();
+					}else {
+						break EXIT;
+					}
+					break;
+				}//switch end
+				
+			}//문자열 반복
+			if(s.isEmpty()) {
+				System.out.printf("#%d 1\n",test);
+			}else {
+				System.out.printf("#%d 0\n",test);
+			}
+		}
+	}
+}
---
+++ KimMinShik/CodingStudy/src/Solution_SWEA_4789_성공적인공연기획.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_SWEA_4789_성공적인공연기획 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int test_case = 1; test_case<=T;test_case++) {
+			String st = br.readLine();
+			int[] p = new int[st.length()];
+			int cnt = 0;
+			int fin = 0;
+			for(int i = 0 ; i<st.length(); i++) {
+				if(st.charAt(i)-'0'>1) {
+					cnt += st.charAt(i)-'0'-1;
+				}else if(st.charAt(i)-'0'==0) {
+					if(cnt!=0) {
+						cnt--;
+					}else {
+						fin++;
+					}
+				}
+			}
+			cnt = Math.max(0, cnt);
+			System.out.printf("#%d %d\n",test_case,fin);
+		}
+	}
+}
