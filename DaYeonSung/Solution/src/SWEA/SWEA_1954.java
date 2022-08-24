6b5f234925342c60884e2f9a72fc3c701191bd76
yoni <sdy32@naver.com>
2022-08-24 23:12:41 +0900

    SWEA_1954_달팽이 숫자

--- DaYeonSung/Solution/src/SWEA/SWEA_1954.java
+++ DaYeonSung/Solution/src/SWEA/SWEA_1954.java
@@ -3,10 +3,8 @@ package SWEA;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 // SWEA 1954. 달팽이 숫자
-
 public class SWEA_1954 {
 
 	public static void main(String[] args) throws IOException {
